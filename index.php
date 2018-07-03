9fae8d5a1a9ec3f6ac04c5aef363dd6b2c184c58
messikiller <messikiller@aliyun.com>
2018-07-03 08:47:02 +0800

    fix

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
 
 session_start();
+$db = require_once('inc/db.inc.php');
 
 if (empty($_SESSION['user'])) {
     header('location:login.html');
     exit();
 }
 
-$user = $_SESSION['user'];
+$uid = $_SESSION['user']['id'];
+
+$user = $db->getRow("select * from system_member where id='{$uid}'");
 
 require_once(__DIR__.'/tpl/index.tpl');
